4b3a920b00e3f260cd62e588e64182869286bdee
nahuelmina <nahuelmina@gmail.com>
2021-09-22 10:28:13 -0300

    Configuración de puertos para subir a Heroku

--- app.js
+++ app.js
@@ -17,4 +17,4 @@ app.get('/login', (req, res) => {
 	res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
